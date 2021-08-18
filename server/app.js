0b1ef7c05b27cdf4957a1f2acba011bdc0f0b4b3
devon <dvowen@cox.net>
2021-08-17 21:20:44 -0500

    changed express routes

--- server/app.js
+++ server/app.js
@@ -45,9 +45,9 @@ mongoose.connect(dbRoute, {
 
 import path from "path";
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "./client/build")));
+  app.use(express.static(path.join(__dirname, "../client/build")));
 
   app.get("*", function (req, res) {
-   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+   res.sendFile(path.join(__dirname, "../client/build/index.html"));
   });
 }
