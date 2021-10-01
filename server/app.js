2989a4c2187b22b3e2e07157bc2fe3c919c9b136
devon <dvowen@cox.net>
2021-09-30 21:40:51 -0500

    inventory database decided and begun

--- server/app.js
+++ server/app.js
@@ -9,20 +9,20 @@ import bodyParser from 'body-parser';
 import cookieparser from 'cookie-parser'
 import mongoose from 'mongoose'
 // import passport from 'passport'
-const dbRoute = `mongodb://${process.env.DBUSER}:${process.env.DBPASSWORD}@51.222.231.153:27017/${process.env.DBNAME}?authSource=${process.env.DBNAME}`
+const dbRoute = `mongodb://erpdemo_main:AD8wgtbPI3a5@51.222.231.153:27017/erpdemo_main?authSource=erpdemo_main`
 
 app.use(cookieparser())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(session({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
 
-app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", 'http://localhost:3000');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", 'http://localhost:3000');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
 });
 // passportConfig(app)
 
@@ -31,8 +31,8 @@ app.use('/api', routes);
 console.log(dbRoute)
 // Database connection
 mongoose.connect(dbRoute, {
-    useCreateIndex: true,
-    useFindAndModify: false
+  useCreateIndex: true,
+  useFindAndModify: false
 });
 
 import path from 'path';
@@ -40,16 +40,16 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-  app.use( '/', express.static(path.join(__dirname, "../client/build")));
+app.use('/', express.static(path.join(__dirname, "../client/build")));
 
-  app.get("*", function (req, res) {
-   res.sendFile(path.join(__dirname, "../client/build/index.html"));
-  });
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 
-    app.listen(port, function() {
-      console.log("App listening on PORT " + port);
-    });
+app.listen(port, function () {
+  console.log("App listening on PORT " + port);
+});
 
 //     import passcode from './models/passcodes.js'
 
--- server/models/inventory.js
+++ server/models/inventory.js
@@ -2,7 +2,16 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 const inventorySchema = new Schema({
-
+    name: String,
+    type: {type: Schema.Types.ObjectId, ref: 'category'},
+    purchaseID: {type: Schema.Types.ObjectId, ref: 'purchaseOrders'},
+    soldTo: {type: Schema.Types.ObjectId, ref: 'customers'},
+    description: String,
+    additionalInfo: String,
+    dateEntered: Date,
+    inStock: Boolean,
+    cost: Number,
+    sku: String
 });
 
 
